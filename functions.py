8d5b3088c19f42560ca6607bad246d939756270c
Pako <paceko@outlook.com>
2016-04-23 16:51:58 -0700

    add multiples

--- functions.py
+++ functions.py
@@ -8,3 +8,10 @@ please_print()
 def my_name(name):
     print "Hi", name
 my_name("Bas")
+
+#Write a function that takes two integers and multiplies them together.
+#Print the result inside of the function.
+
+def multiples(integer_one, integer_two):
+    print integer_one * integer_two
+
